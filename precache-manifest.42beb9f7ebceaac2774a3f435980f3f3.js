4e6fb95a7a392c62d193c97838e7186f36265851
aleksandrakostina <60540168+aleksandrakostina@users.noreply.github.com>
2020-10-20 10:37:53 +0300

    Updates

---
+++ precache-manifest.42beb9f7ebceaac2774a3f435980f3f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c002c2af083f65077cdd542f34829119",
+    "url": "/test_emphasoft/index.html"
+  },
+  {
+    "revision": "6641d8bf1f66d84f2beb",
+    "url": "/test_emphasoft/static/css/main.fb4adb70.chunk.css"
+  },
+  {
+    "revision": "e713cd8da0afbde75621",
+    "url": "/test_emphasoft/static/js/2.a2ec8652.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/test_emphasoft/static/js/2.a2ec8652.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6641d8bf1f66d84f2beb",
+    "url": "/test_emphasoft/static/js/main.08f59e6f.chunk.js"
+  },
+  {
+    "revision": "15023fd5a5aa1ae3b7ec",
+    "url": "/test_emphasoft/static/js/runtime-main.d688091b.js"
+  }
+]);
